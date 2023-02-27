72022caf5183d2c8484e33fdeaa9a1721179d10e
Paula Waite <pawaite@madisoncollege.edu>
2023-02-27 16:20:22 -0600

    One more useState demo - loading!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import { API } from 'aws-amplify';
@@ -11,6 +10,9 @@ const App = () => {
     // Create additional state to hold user input for limit and start properties
     const [input, updateInput] = useState({ limit: 5, start: 0 });
 
+    // Create a variable for loading
+    const [loading, updateLoading] = useState(true);
+
     // Create a new function to allow users to update the input values
     function updateInputValues(type, value) {
         updateInput({ ...input, [type]: value });
@@ -18,9 +20,11 @@ const App = () => {
 
     // Define function to call API
     const fetchCoins = async() =>  {
+        updateLoading(true);
         const { limit, start } = input;
         const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`);
-      updateCoins(data.coins)
+        updateCoins(data.coins);
+        updateLoading(false);
     }
 
     // Call fetchCoins function when component loads
@@ -39,8 +43,9 @@ const App = () => {
              placeholder="limit"
          />
          <button onClick={fetchCoins}>Fetch Coins</button>
-        {
-          coins.map((coin, index) => (
+         {loading && <h2>Loading...</h2>}
+         {
+           !loading && coins.map((coin, index) => (
             <div key={index}>
               <h2>{coin.name} - {coin.symbol}</h2>
               <h5>${coin.price_usd}</h5>
