82935950454fbf11cbd7cf72172d61e5d480911f
Paula Waite <pawaite@madisoncollege.edu>
2023-02-27 16:14:04 -0600

    adding usestate and updating the function to make the limit and start…

--- src/App.js
+++ src/App.js
@@ -8,9 +8,18 @@ const App = () => {
     // Create coins variable and set to empty array
     const [coins, updateCoins] = useState([]);
 
+    // Create additional state to hold user input for limit and start properties
+    const [input, updateInput] = useState({ limit: 5, start: 0 });
+
+    // Create a new function to allow users to update the input values
+    function updateInputValues(type, value) {
+        updateInput({ ...input, [type]: value });
+    }
+
     // Define function to call API
     const fetchCoins = async() =>  {
-      const data = await API.get('cryptoapi', '/coins')
+        const { limit, start } = input;
+        const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`);
       updateCoins(data.coins)
     }
 
