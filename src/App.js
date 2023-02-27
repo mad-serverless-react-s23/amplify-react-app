4cb1c95aaaf1d07ca7ddfbe2f12854ea156f0d19
Paula Waite <pawaite@madisoncollege.edu>
2023-02-27 16:02:41 -0600

    updating to a const - more common today in react

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ const App = () => {
     // Create coins variable and set to empty array
     const [coins, updateCoins] = useState([]);
 
-    // Define function to all API
-    async function fetchCoins() {
+    // Define function to call API
+    const fetchCoins = async() =>  {
       const data = await API.get('cryptoapi', '/coins')
       updateCoins(data.coins)
     }
